f8737fbccdc6bcb1b00f84146311eec3adf3f606
Colin <colin.diesh@gmail.com>
2018-02-26 22:04:17 -0500

    Update package

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+/*
+ * (C) Copyright IBM Corp. 2012, 2016 All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var DojoWebpackPlugin = require("dojo-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+	context: __dirname,
+	entry: "src/JBrowse/init",
+	output: {
+		path: path.join(__dirname, "release"),
+		publicPath: "release/",
+		pathinfo: true,
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{ test: /\.(png)|(gif)$/, loader: "url-loader?limit=100000" }
+		]
+	},
+	plugins: [
+		new DojoWebpackPlugin({
+			loaderConfig: require("./loaderConfig"),
+			environment: {dojoRoot: "release"},	// used at run time for non-packed resources (e.g. blank.gif)
+			buildEnvironment: {dojoRoot: "node_modules"}, // used at build time
+			locales: ["en"]
+		}),
+
+		// Copy non-packed resources needed by the app to the release directory
+		new CopyWebpackPlugin([{
+			context: "node_modules",
+			from: "dojo/resources/blank.gif",
+			to: "dojo/resources"
+		}]),
+
+		// For plugins registered after the DojoAMDPlugin, data.request has been normalized and
+		// resolved to an absMid and loader-config maps and aliases have been applied
+		new webpack.NormalModuleReplacementPlugin(/^dojox\/gfx\/renderer!/, "dojox/gfx/canvas"),
+		new webpack.NormalModuleReplacementPlugin(
+			/^css!/, function(data) {
+				data.request = data.request.replace(/^css!/, "!style-loader!css-loader!less-loader!")
+			}
+		),
+
+		new webpack.optimize.UglifyJsPlugin({
+			output: {comments: false},
+			compress: {warnings: false},
+			sourceMap: true
+		})
+	],
+	resolveLoader: {
+		modules: ["node_modules"]
+	},
+	devtool: "#source-map",
+	node: {
+		process: false,
+		global: false
+	}
+};
